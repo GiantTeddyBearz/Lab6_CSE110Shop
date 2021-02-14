97357f9fa07ee54a017ce8a9627ac08dd928cc2a
GiantTeddyBearz <giantteddybear1@gmail.com>
2021-02-13 23:57:12 -0800

    finish lab

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,144 @@
 // product-item.js
 
+// <li class="product">
+//     <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
+//     <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
+//     <p class="price">$109.95</p>
+//     <button onclick="alert('Added to Cart!')">Add to Cart</button>
+// </li>
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor () {
+    super(); //always call super?
+    
+    //create shadow dom
+    const shadow = this.attachShadow({mode: 'open'});
+
+    //CREATE NEEDED ELEMENTS
+    //List
+    const liElem = document.createElement('li');
+    liElem.setAttribute('class','product');
+
+    //Img
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('width', '200');
+
+    //Title
+    const pElem1 = document.createElement('p');
+    pElem1.setAttribute('class', 'title');
+    
+    //Price
+    const pElem2 = document.createElement('p');
+    pElem2.setAttribute('class', 'price');
+
+    
+    //Button
+    const buttonElem = document.createElement('button');
+    buttonElem.textContent = 'Add to Cart';
+    buttonElem.setAttribute('onClick','onClick(event)');
+
+    const style = document.createElement('style');
+    style.textContent = `
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+    `
+    shadow.appendChild(style);
+    shadow.appendChild(liElem);
+    liElem.appendChild(imgElem);
+    liElem.appendChild(pElem1);
+    liElem.appendChild(pElem2);
+    liElem.appendChild(buttonElem);
+
+  }
+  connectedCallback() {
+    fill(this);
+  }
 }
 
 customElements.define('product-item', ProductItem);
+
+function fill(elem) { // this was painful to figure out... like why...
+  //filling in elements
+  //img
+  console.log('using fill')
+  const shadow = elem.shadowRoot;
+  let imgElem = shadow.querySelector('img');
+  let pElem1 = shadow.querySelector('.title');
+  let pElem2 = shadow.querySelector('.price');
+  let liElem = shadow.querySelector('li');
+
+  const imgUrl = elem.getAttribute('img');
+  const imgAlt = elem.getAttribute('img-alt')
+  imgElem.src = imgUrl;
+  imgElem.alt = imgAlt;
+
+  //pElem1 - title
+  const text = elem.getAttribute('item-title');
+  pElem1.textContent = text;
+
+  //pElem2 - price
+  const price = '$' + elem.getAttribute('price');
+  pElem2.textContent = price;
+
+  liElem.setAttribute('id', elem.getAttribute('id'));
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,74 @@
 // Script.js
-
+const myStorage = window.localStorage
+let objArray;
+let count = 0;
+let cart = [];
+if (myStorage.getItem('cart') === null){
+  myStorage.setItem('cart', cart);
+}
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    if (myStorage.getItem('array') === null){
+      myStorage.setItem('array', JSON.stringify(data));
+      console.log(myStorage.getItem('array'));
+    }
+    const itemData = myStorage.getItem('array');
+    objArray = [...JSON.parse(itemData)];
+    console.log(objArray);
+    objArray.forEach(obj => {
+      const productItem = document.createElement('product-item');
+      productItem.setAttribute('id', obj.id);
+      productItem.setAttribute('img', obj.image);
+      productItem.setAttribute('img-alt', obj.title);
+      productItem.setAttribute('item-title', obj.title);
+      productItem.setAttribute('price', obj.price);
+      document.getElementById('product-list').appendChild(productItem);
+    }
+    )
+    updatePageByCart();
+  });
 });
+
+const onClick = e => {
+  e.preventDefault();
+  if (e.target.textContent === 'Add to Cart'){
+    alert('Added to cart!');
+    e.target.textContent = 'Remove from Cart';
+    e.target.parentElement.setAttribute('inCart', 'true');
+    count++;
+    cart.push(e.target.parentElement.getAttribute('id'));
+    console.log(e.target.parentElement.getAttribute('id'));
+  } else {
+    alert('Removed from cart!');
+    e.target.textContent = 'Add to Cart';
+    e.target.parentElement.setAttribute('inCart', 'false');
+    count--;
+    cart = cart.filter(item => item !== e.target.parentElement.getAttribute('id'));
+  }
+  updateCart();
+}
+
+const updatePageByCart = () => {
+  if(myStorage.getItem('cart').length != 0){
+    cart = myStorage.getItem('cart').split(',');
+  }
+  console.log(cart);
+  const Items = [...document.querySelectorAll('li')];
+  Items.forEach(item => {
+    console.log(item.getAttribute('id'));
+    if (cart.includes(item.getAttribute('id'))){
+      item.querySelector('button').textContent = 'Remove from Cart';
+    }
+  })
+  count = cart.length;
+  document.getElementById('cart-count').textContent = count;
+}
+const updateCart = () => {
+  const cartCount = document.getElementById('cart-count');
+  cartCount.textContent = count;
+  myStorage.setItem('cart', cart);
+  console.log(myStorage.getItem('cart'));
+}
