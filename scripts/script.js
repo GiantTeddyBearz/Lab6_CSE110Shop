2f7db7cebe6e080ff5ff23d2ec438ec3ed3c1278
GiantTeddyBearz <giantteddybear1@gmail.com>
2021-02-14 00:58:20 -0800

    Part 6 fixed, commit late

--- scripts/script.js
+++ scripts/script.js
@@ -18,9 +18,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const itemData = myStorage.getItem('array');
     objArray = [...JSON.parse(itemData)];
     console.log(objArray);
+    updatePageByCart();
+    console.log(cart);
     objArray.forEach(obj => {
       const productItem = document.createElement('product-item');
       productItem.setAttribute('id', obj.id);
+      if (cart.includes(productItem.getAttribute('id'))){
+        productItem.shadowRoot.querySelector('button').textContent = 'Remove from Cart';
+      }
       productItem.setAttribute('img', obj.image);
       productItem.setAttribute('img-alt', obj.title);
       productItem.setAttribute('item-title', obj.title);
@@ -28,7 +33,6 @@ window.addEventListener('DOMContentLoaded', () => {
       document.getElementById('product-list').appendChild(productItem);
     }
     )
-    updatePageByCart();
   });
 });
 
@@ -56,16 +60,18 @@ const updatePageByCart = () => {
     cart = myStorage.getItem('cart').split(',');
   }
   console.log(cart);
-  const Items = [...document.querySelectorAll('li')];
-  Items.forEach(item => {
-    console.log(item.getAttribute('id'));
-    if (cart.includes(item.getAttribute('id'))){
-      item.querySelector('button').textContent = 'Remove from Cart';
-    }
-  })
+  // const Items = document.querySelectorAll('product-item');
+  // console.log(Items);
+  // Items.forEach(item => {
+  //   console.log(item.getAttribute('id'));
+  //   if (cart.includes(item.getAttribute('id'))){
+  //     item.querySelector('button').textContent = 'Remove from Cart';
+  //   }
+  // })
   count = cart.length;
   document.getElementById('cart-count').textContent = count;
 }
+
 const updateCart = () => {
   const cartCount = document.getElementById('cart-count');
   cartCount.textContent = count;
